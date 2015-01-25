5194713118ed31a4eecc3b02925da4d2b4f9b87e
Kevin Grandon <kevingrandon@yahoo.com>
2015-01-25 12:38:51 -0800

    Add standard dist export

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var to5 = require('gulp-6to5');
+var rename = require('gulp-rename');
 
-gulp.task('to5', function () {
+gulp.task('to5-dist-modules', function () {
 	var files = ['./fxos_apps.js'];
 	return gulp.src(files)
 		.pipe(to5({
 			modules: 'amd'
 		}))
+		.pipe(rename('build.amd.js'))
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['to5'], function () {
+gulp.task('to5-dist-window', function () {
+	var files = ['./fxos_apps.js'];
+	return gulp.src(files)
+		.pipe(to5())
+		.pipe(rename('build.standard.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['to5-dist-modules', 'to5-dist-window'], function () {
 	console.log('all done.');
 });
